abad04950d6c2306d3eca7ab785ec78f4d0b9421
Lilla <korosililla@gmail.com>
2019-09-16 12:20:28 +0200

    images got hover

--- js/script.js
+++ js/script.js
@@ -1,64 +1,68 @@
-const imageWrapperDiv = document.querySelector('#images-wrapper');
-console.log(imageWrapperDiv);
-
-
-// Iframe
-// When clicked on 'See codes' - it changes src to github
-const iframe = document.querySelector('iframe');
-console.log(iframe);
-
-const iframeButton = document.querySelector('#iframe-btn');
-iframeButton.addEventListener('click', function() {
-    window.open('https://github.com/Laila1222/Laila1222-github.io/blob/master/hyf-homework/html-css/week2/index.html');
-})
+const jsImageWrapperDiv = document.querySelector('#images-wrapper-js');
+const cssImageWrappedDiv = document.querySelector('#images-wrapper-css');
 
 
 
-console.log(sites);
 
-function renderModals (sites) {
-    sites.forEach((site) => {
-        console.log(site.id);
-        const imageDiv = document.createElement('div');
-        imageDiv.className = 'col';
-        imageWrapperDiv.appendChild(imageDiv);
-        imageDiv.innerHTML = `
-        <a data-toggle="modal" href=#${site.id}><img src="./images/P8170178.JPG" alt=images"" class="d-block w-100"></a>
-        <div class="modal fade" id="${site.id}" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" >
-        <div class="modal-dialog modal-lg" role="document">
-            <!-- Content -->
-            <div class="modal-content">
-                <!-- Body -->
-                <div class="modal-body">
-        
-                    <div  class="embed-responsive embed-responsive-16by9 z-depth-1-half inner-modal-body">
+
+
+
+function appendInnerHtml (el, arrayEl) {
+    el.innerHTML = `
+                <a data-toggle="modal"  href=#${arrayEl.id}><img src="${arrayEl.image}" alt="images" class="w-75 shadow"></a>
+                <div class="modal fade" id="${arrayEl.id}" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" >
+                <div class="modal-dialog modal-lg" role="document">
+                    <!-- Content -->
+                    <div class="modal-content">
+                        <!-- Body -->
+                        <div class="modal-body">
+
+                            <div  class="embed-responsive embed-responsive-16by9 z-depth-1-half inner-modal-body">
+                                    
+                                <iframe class="embed-responsive-item" allowfullscreen  src="${arrayEl.url}" frameborder="0"></iframe>
+                            </div>
                             
-                        <iframe class="embed-responsive-item" allowfullscreen  src="${site.url}" frameborder="0"></iframe>
+                        </div> <!-- body -->
+                        
+                        <!-- Footer -->
+                        <div class="modal-footer justify-content-center">
+                            <span class="mr-4">blablaa</span>
+                            <button id="iframe-codes-btn" onclick="openCodes('${arrayEl.codesUrl}')" class="btn btn-info btn-floating btn-sm" >See Codes on GitHub</button>
+                            <button id="iframe-same-new-tab" onclick="openNewTab('${arrayEl.url}')" class="btn btn-info btn-floating btn-sm">Open in new tab</button>
+
+                        </div>
+
                     </div>
                     
-                </div> <!-- body -->
-                
-                <!-- Footer -->
-                <div class="modal-footer justify-content-center">
-                    <span class="mr-4">blablaa</span>
-                    <button id="iframe-codes-btn"  class="btn-floating btn-sm btn-fb" >See Codes on GitHub</button>
-                    <button id="iframe-same-new-tab" class="btn-floating btn-sm btn-fb">Open in new tab</button>
-
                 </div>
+                    
 
             </div>
-            
-        </div>
-            
+            `
 
-    </div>
-        `
-      
-       
-        
+}
+
+
+
+function renderModals (sites, div) {
+    sites.forEach((site) => {
+        const imageDiv = document.createElement('div');
+        imageDiv.className = "col-12 col-md-6 flash-hover py-5";
+        div.appendChild(imageDiv);
+        appendInnerHtml(imageDiv, site);  
     })
-    };
+};
 
-renderModals(sites);
+const openCodes = (link) => {
+    window.open(link, '_blank');
+    // console.log(link);
+}
+
+const openNewTab = (link) => {
+    window.open(link, '_blank');
+    // console.log(link);
+}
 
 
+renderModals(sitesJavaScript, jsImageWrapperDiv);
+renderModals(sitesHmlCss, cssImageWrappedDiv);
--- js/sites.js
+++ js/sites.js
@@ -1,38 +1,52 @@
-const sites = [
-    {
-        "name": "Weather app",
-        "id": "weather",
-        "url":   "https://laila1222.github.io/Laila1222-github.io/hyf-homework/js-projects/weatherApp/Wheater%20app%20with%20local%20storage/index.html",
-        "codesUrl": "https://github.com/Laila1222/Laila1222-github.io/blob/master/hyf-homework/html-css/week2/index.html",
-        "image": "C:\Users\Lillus\Desktop\plan-portfolio\small-images\weather.jpg"
-    },
+const sitesHmlCss = [
+
     {
         "name": "office",
         "id": "office",
-        "url": "https://laila1222.github.io/Laila1222-github.io/hyf-homework/html-css/week2/index.html",
+        "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/html-css/week2/Webpage%20with%20css/index.html",
         "codesUrl": "https://github.com/Laila1222/Laila1222-github.io/blob/master/hyf-homework/html-css/week2/index.html",
-        "image":
+        "image": "../images/office.jpg"
     },
     {
         "name": "travel-goat",
         "id": "travelGoat",
-        "url": "https://laila1222.github.io/Laila1222-github.io/hyf-homework/html-css/week3/index1.html",
-        "codesUrl": "https://github.com/Laila1222/Laila1222-github.io/blob/master/hyf-homework/html-css/week2/index.html",
-        "image":
+        "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/html-css/week3/index2.html",
+        "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/html-css/week3/index2.html",
+        "image": "../images/travel-goat.jpg"
     },
-    {
-        "name": "emoji-helper",
-        "id": "emojiHelper",
-        "url": "https://laila1222.github.io/Laila1222-github.io/hyf-homework/html-css/week2/index.html",
-        "codesUrl": "https://github.com/Laila1222/Laila1222-github.io/blob/master/hyf-homework/html-css/week2/index.html",
-        "image": "C:\Users\Lillus\Desktop\plan-portfolio\small-images\emoji-helper.jpeg"
-    },
-    {
-        "name": "office",
-        "id": "office",
-        "url": "https://laila1222.github.io/Laila1222-github.io/hyf-homework/html-css/week2/index.html",
-        "codesUrl": "https://github.com/Laila1222/Laila1222-github.io/blob/master/hyf-homework/html-css/week2/index.html",
-        "image": "C:\Users\Lillus\Desktop\plan-portfolio\small-images\office.jpg"
-    }
+
+
+
+];
+
+const sitesJavaScript = [
+{
+    "name": "Weather app",
+    "id": "weather",
+    "url":   "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/Wheater%20app%20with%20local%20storage/index.html",
+    "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/Wheater%20app%20with%20local%20storage/index.html",
+    "image": "../images/weather.jpg"
+},
+{
+    "name": "emoji-helper",
+    "id": "emojiHelper",
+    "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/Emoji-helper/index.html",
+    "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/Emoji-helper/index.html",
+    "image": "../images/emoji-helper.jpeg"
+},
+{
+    "name": "Fastest presser",
+    "id": "presserGame",
+    "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/Presser%20version2/index.html",
+    "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/Presser%20version2/index.html",
+    "image": "../images/presser-game.jpg"
+},
+{
+    "name": "Shopping cart",
+    "id": "shoppingCart",
+    "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/ShoppingCart/index.html",
+    "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/ShoppingCart/index.html",
+    "image": "../images/shoppingcart.jpg"
+}
 
 ];
