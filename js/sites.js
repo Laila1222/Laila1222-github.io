e03c6e6cee58aceba6cc5c6e6bcc61f881418382
Lilla <korosililla@gmail.com>
2019-09-16 12:26:52 +0200

    fixed path for images

--- js/sites.js
+++ js/sites.js
@@ -5,14 +5,14 @@ const sitesHmlCss = [
         "id": "office",
         "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/html-css/week2/Webpage%20with%20css/index.html",
         "codesUrl": "https://github.com/Laila1222/Laila1222-github.io/blob/master/hyf-homework/html-css/week2/index.html",
-        "image": "../images/office.jpg"
+        "image": "./images/office.jpg"
     },
     {
         "name": "travel-goat",
         "id": "travelGoat",
         "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/html-css/week3/index2.html",
         "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/html-css/week3/index2.html",
-        "image": "../images/travel-goat.jpg"
+        "image": "./images/travel-goat.jpg"
     },
 
 
@@ -25,28 +25,28 @@ const sitesJavaScript = [
     "id": "weather",
     "url":   "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/Wheater%20app%20with%20local%20storage/index.html",
     "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/Wheater%20app%20with%20local%20storage/index.html",
-    "image": "../images/weather.jpg"
+    "image": "./images/weather.jpg"
 },
 {
     "name": "emoji-helper",
     "id": "emojiHelper",
     "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/Emoji-helper/index.html",
     "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/Emoji-helper/index.html",
-    "image": "../images/emoji-helper.jpeg"
+    "image": "./images/emoji-helper.jpeg"
 },
 {
     "name": "Fastest presser",
     "id": "presserGame",
     "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/Presser%20version2/index.html",
     "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/Presser%20version2/index.html",
-    "image": "../images/presser-game.jpg"
+    "image": "./images/presser-game.jpg"
 },
 {
     "name": "Shopping cart",
     "id": "shoppingCart",
     "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/javascript/ShoppingCart/index.html",
     "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/javascript/ShoppingCart/index.html",
-    "image": "../images/shoppingcart.jpg"
+    "image": "./images/shoppingcart.jpg"
 }
 
 ];
