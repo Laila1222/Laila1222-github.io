4b702fb553bccc63d7ced115bf7c5a8c8df875cd
Lilla <korosililla@gmail.com>
2019-09-17 20:08:38 +0200

    fixed links

--- js/sites.js
+++ js/sites.js
@@ -9,7 +9,7 @@ const sitesHtmlCss = [
     {
         "name": "telmore",
         "id": "telmore",
-        "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/html-css/html-css/challenge%20project/assignment.html",
+        "url": "https://laila1222.github.io/lillaKorosi.github.io/projects/html-css/challenge%20project/assignment.html",
         "codesUrl": "https://github.com/Laila1222/lillaKorosi.github.io/blob/master/projects/html-css/challenge%20project/assignment.html",
         "image": "./images/telmore2.jpg"
     },
